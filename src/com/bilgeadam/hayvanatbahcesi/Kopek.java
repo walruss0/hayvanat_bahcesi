0cfd8f5c8c3eff0dab8bb5ac0faedd2e150479a3
Onur Yildirim <onuryildirim5425@gmail.com>
2021-11-13 13:10:52 +0300

    inheritance

--- src/com/bilgeadam/hayvanatbahcesi/Kopek.java
+++ src/com/bilgeadam/hayvanatbahcesi/Kopek.java
@@ -3,4 +3,27 @@ package com.bilgeadam.hayvanatbahcesi;
 //süperclass'ın özelliklerini subclass alıyor.
 //extends ile sağlanır.
 public class Kopek extends Hayvan{
+    private String kopekCins;
+
+    public String getKopekCins() {
+        return kopekCins;
+    }
+
+    public void setKopekCins(String kopekCins) {
+        this.kopekCins = kopekCins;
+    }
+
+    //Method overriding
+    //süperclass methodunu kendine özgü tekrar yazıyoruz.
+    public void sesCikar(){
+        System.out.println("Hav Hav Hav");
+    }
+
+    @Override
+    public String toString() {
+        return "Ad: "+getAd()+"\n"+
+                "Ağırlık: "+getAgirlik()+"\n"+
+                "Uzunluk: "+getUzunluk()+"\n"+
+                "Cins: "+getKopekCins();
+    }
 }
--- src/com/bilgeadam/hayvanatbahcesi/test/KopekTest.java
+++ src/com/bilgeadam/hayvanatbahcesi/test/KopekTest.java
@@ -6,5 +6,13 @@ public class KopekTest {
     public static void main(String[] args) {
         Kopek kopek1 = new Kopek();
         kopek1.sesCikar();
+
+        kopek1.setKopekCins("Doberman");
+        System.out.println(kopek1.getKopekCins());
+        kopek1.setAgirlik(20);
+        kopek1.setAd("Çomak");
+        kopek1.setUzunluk(2);
+
+        System.out.println(kopek1);
     }
 }
