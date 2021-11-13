7d19751cde87af7dfad9ad76c648dfc6f5016daa
Onur Yildirim <onuryildirim5425@gmail.com>
2021-11-13 11:42:08 +0300

    ooj

---
+++ src/com/bilgeadam/hayvanatbahcesi/Hayvan.java
@@ -0,0 +1,46 @@
+package com.bilgeadam.hayvanatbahcesi;
+//Encapsulation = Information Hidding = Bilgi Gizleme
+//Değişkenler private olur, get-set methodlarıyla
+public class Hayvan {
+    private String  ad;
+    private double  agirlik;
+    private double  uzunluk;
+
+    //contructor
+    public Hayvan() {
+        ad = "";
+        agirlik = 0;
+        uzunluk = 0;
+    }
+
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public double getAgirlik() {
+        return agirlik;
+    }
+
+    public void setAgirlik(double agirlik) {
+        this.agirlik = agirlik;
+    }
+
+    public double getUzunluk() {
+        return uzunluk;
+    }
+
+    public void setUzunluk(double uzunluk) {
+        this.uzunluk = uzunluk;
+    }
+
+    public void sesCikar(){
+            System.out.println("--------");
+    }
+
+
+}
---
+++ src/com/bilgeadam/hayvanatbahcesi/test/HayvanTest.java
@@ -0,0 +1,15 @@
+package com.bilgeadam.hayvanatbahcesi.test;
+
+import com.bilgeadam.hayvanatbahcesi.Hayvan;
+
+public class HayvanTest {
+
+    public static void main(String[] args) {
+    Hayvan h1 = new Hayvan();
+    h1.sesCikar();
+    }
+
+
+
+
+}
